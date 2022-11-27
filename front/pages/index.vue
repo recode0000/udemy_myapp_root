c1bed01387464c821dadd43a9f8e9c76f24a2f58
Rikito Ikeda <rikito_ikeda@commude.co.jp>
2022-11-27 12:36:48 +0900

    [fix]

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,10 +1,33 @@
-<template lang="pug">
-  tutorial
+<template>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage'
-})
+export default {
+  data () {
+    return {
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg () {
+      this.$axios.$get('/api/v1/hello')
+        .then(res => this.msgs.push(res))
+    }
+  }
+}
+</script>
